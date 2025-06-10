1c0f189053430b3556281d10eafae3543f37f763
enric-bazz <enrico.bazzacco02@outlook.it>
2025-06-11 01:40:27 +0200

    minor adjustments

--- MyScripts/mytangram_test.py
+++ MyScripts/mytangram_test.py
@@ -17,26 +17,41 @@ tg.pp_adatas(adata_sc, adata_st)
 
 
 # run mapping
-mode = "cells"
+mode = "constrained"
 target_count = np.round(len(adata_st.var.index)/3)
 ad_map = tg.map_cells_to_space(adata_sc, adata_st, mode=mode, target_count=target_count,
                                density_prior='rna_count_based', num_epochs=100,device='cpu',
-                               lambda_d=1,
+                               lambda_d=0,
                                lambda_g1=1,
-                               lambda_g2=1,
-                               lambda_r=1,
-                               lambda_count=1,
-                               lambda_f_reg=1
+                               lambda_g2=0,
+                               lambda_r=0,
+                               lambda_count=0,
+                               lambda_f_reg=0
                                )
+#lambda_d cannot be 0, if it is passed as 0 it is set to 1 (line 287 mapping_utils)
 
 # plot loss
 tg.plot_loss_terms(adata_map=ad_map, log_scale=False)
 
 # plot filter colormap
-if mode == "constrained":
-    tg.plot_filter_weights(ad_map)
+#if mode == "constrained":
+ #   tg.plot_filter_weights(ad_map)
 
-ad_ge = tg.project_genes(ad_map, adata_sc)
+ad_map_2 = tg.map_cells_to_space(adata_sc, adata_st, mode=mode, target_count=target_count,
+                               density_prior='rna_count_based', num_epochs=100,device='cpu',
+                               lambda_d=1,
+                               lambda_g1=1,
+                               lambda_g2=1,
+                               lambda_r=0.0003,
+                               lambda_count=0.005,
+                               lambda_f_reg=0.01
+                               )
+
+
+tg.plot_loss_terms(adata_map=ad_map_2, log_scale=False)
+
+
+#ad_ge = tg.project_genes(ad_map, adata_sc)
 
 cv_results = tg.cross_validate(
         adata_sc,
@@ -54,7 +69,8 @@ cv_results = tg.cross_validate(
         num_epochs=1000,
         device="cpu",
         learning_rate=0.1,
-        cv_mode="10fold",
+        cv_mode="kfold",
+        cv_k=3,
         density_prior=None,
         random_state=None,
         verbose=False,
@@ -66,7 +82,6 @@ cv_results2 = tg.cross_validate(
         adata_st,
         cluster_label=None,
         mode="cells",
-        scale=False,
         lambda_d=0.5,
         lambda_g1=1,
         lambda_g2=0.5,
@@ -77,13 +92,16 @@ cv_results2 = tg.cross_validate(
         num_epochs=1000,
         device="cpu",
         learning_rate=0.1,
-        cv_mode="10fold",
-        density_prior=None,
+        cv_mode="kfold",
+        cv_k=3,
+        density_prior='uniform',
         random_state=None,
         verbose=False,
         metrics=["SSIM", "PCC", "JS"]
 )
 
+print(cv_results)
+print(cv_results2)
 
 
 
